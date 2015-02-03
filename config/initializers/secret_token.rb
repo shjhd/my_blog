a67a603be30e2badfd6881fb2f9f493e0ee18e36
xtunes <xtunes@xtunes1312.local>
2015-02-03 18:04:37 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'f53cdaeac27f0182e69511a89193949b969cfb110e071eb0e711c5ee921fcca45797d324691d839706e1ba9d04be8edc152b2d1b975293d7d13d6c3243f0b55e'
