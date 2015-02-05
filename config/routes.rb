b8a0eec296add0507677130f56df8b0964a9fe82
xtunes <xtunes@xtunes1312.local>
2015-02-05 17:51:41 +0800

    创建Sessions控制器

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ MyBlog::Application.routes.draw do
   resources :users
 
   resources :posts
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'posts#index'
   match '/signup',  to: 'users#new'
